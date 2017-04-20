95dabe9df12704ccb2305a178334ad6f2856c62b
Infatum <qwertty459@gmail.com>
2017-04-21 01:34:11 +0300

    added checking for digits in directories names

--- main.cpp
+++ main.cpp
@@ -3,18 +3,21 @@
 #include <vector>
 #include <unistd.h>
 #include <string>
+#include <algorithm>
+#include <cctype>
 #include <cstring>
 
 std::vector<std::string> get_dirs(const char *dir_name);
+bool has_any_digits(const std::string &s);
 
 int main() {
     auto v = get_dirs("/proc");
     for (int i = 0; i < v.size(); ++i) {
         if (i == v.size()-1) {
-            std::cout << v[i] << "." << std::endl;
+            std::cout <<  "\t" << v[i] << "." << std::endl;
             break;
         }
-        std::cout << v[i] << ", ";
+        std::cout << "\t" << v[i] << ",\t\t ";
     }
     return 0;
 }
@@ -28,7 +31,7 @@ std::vector<std::string> get_dirs(const char *dir_name)
     if ((dir = opendir(dir_name)) != NULL) {
         while ((ent = readdir(dir)) != NULL) {
             dir_path = "/proc/" + std::string(ent->d_name) + "/cmdline";
-            if (access(dir_path.c_str(), R_OK) != -1)
+            if (access(dir_path.c_str(), R_OK) != -1 && has_any_digits(dir_path))
                 directories_names.push_back(std::string(dir_path));
 
         }
@@ -38,4 +41,9 @@ std::vector<std::string> get_dirs(const char *dir_name)
         std::cerr << "Error reading from directory/file: " << EXIT_FAILURE << std::endl;
     }
     return  directories_names;
+}
+
+bool has_any_digits(const std::string &s)
+{
+    return std::any_of(s.begin(), s.end(), ::isdigit);
 }
